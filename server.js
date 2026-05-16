d3a72ee52e0d6dffcb6b6e2f30a4ff1e7916d7fe
Rawda mohamed <Rawdamohamed618@gmail.com>
2026-05-17 00:50:59 +0300

    fix(server): decouple db connection and adapt architecture for serverless deployment

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 import dotenv from "dotenv";
 dotenv.config();
-import app from "./src/app.js";
+import app, { httpServer } from "./src/app.js";
 import connectDB from "./src/core/config/db.js";
-import { httpServer } from "./src/app.js";
 import { initSocket } from "./src/socket/socket.js";
 import { startCronJobs } from "./src/core/utils/cron.js";
 import { initFirebase } from "./src/core/firebase/firebase.js";
+
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
+const initializeServices = async () => {
   try {
     await connectDB();
     initFirebase();
     initSocket(httpServer);
-    startCronJobs();
-    httpServer.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`);
-    });
   } catch (err) {
-    console.log("Database connection error:", err.message);
+    console.error("Initialization error:", err.message);
   }
 };
 
-startServer();
+initializeServices();
+
+if (process.env.NODE_ENV !== "production") {
+  startCronJobs();
+  httpServer.listen(PORT, () => {
+    console.log(`🚀 Local Server started on port ${PORT}`);
+  });
+}
+
+export default httpServer;

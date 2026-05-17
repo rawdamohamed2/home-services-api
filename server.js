fd9f2afdaab0aa9204cb966df91b88f506549e7b
Rawda mohamed <Rawdamohamed618@gmail.com>
2026-05-18 00:23:12 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import app, { httpServer } from "./src/app.js";
 import connectDB from "./src/core/config/db.js";
 import { initSocket } from "./src/socket/socket.js";
 import { startCronJobs } from "./src/core/utils/cron.js";
 import { initFirebase } from "./src/core/firebase/firebase.js";
 
-const PORT = process.env.PORT || 5000;
-
 const initializeServices = async () => {
   try {
     await connectDB();
+
     initFirebase();
+
     initSocket(httpServer);
+
     startCronJobs();
-    httpServer.listen(PORT, () => {
-      console.log(`🚀 Server fully operational on port ${PORT}`);
-    });
+
+    console.log("🚀 Server initialized");
   } catch (err) {
-    console.error("Initialization error:", err.message);
+    console.error("Initialization error:", err);
   }
 };
 
 initializeServices();
 
-export default httpServer;
+export default app;
